f5bf3f633a4b41ea3b806061427e61dd8c3271ce
Vladimir Ermukhanov <56549819+woksel@users.noreply.github.com>
2021-08-03 14:36:23 +0400

    refactor: removed useless files and directories

--- build/server/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ActionType = void 0;
-var ActionType;
-(function (ActionType) {
-})(ActionType = exports.ActionType || (exports.ActionType = {}));
-//# sourceMappingURL=types.js.map
