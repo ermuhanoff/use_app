304189deea7eda08c377d8f553fa62f0563b6db8
Vladimir Ermukhanov <ermuhanoff@gmail.com>
2021-08-03 14:30:10 +0400

    fix: fixed craco config in some cases

--- craco.config.js
+++ craco.config.js
@@ -1,17 +1,28 @@
-const CracoLessPlugin = require("craco-less");
+const CracoLessPlugin = require("craco-less-plugin");
+const path = require("path");
 
 module.exports = {
   plugins: [
     {
       plugin: CracoLessPlugin,
       options: {
-        lessLoaderOptions: {
-          lessOptions: {
-            modifyVars: { "@base-color": "white"},
-            javascriptEnabled: true,
-          },
+        lessOptions: {
+          modifyVars: { "@base-color": "#2c2c32" },
+          javascriptEnabled: true,
         },
       },
     },
   ],
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname),
+      node: path.resolve(__dirname, "node_modules"),
+      src: path.resolve(__dirname, "src"),
+      styles: path.resolve(__dirname, "src/ui/styles"),
+      store: path.resolve(__dirname, "src/ui/store"),
+      server: path.resolve(__dirname, "src/server"),
+      ui: path.resolve(__dirname, "src/ui"),
+    },
+    extensions: [".js", ".jsx", ".css", ".less", ".tsx", ".ts"],
+  },
 };
